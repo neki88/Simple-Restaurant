113d120b80e0931f9b066c5b693b44599aebd469
Beoneki <neki41@gmail.com>
2022-08-23 08:55:50 +0200

    nn

---
+++ javaSript.js
@@ -0,0 +1,30 @@
+const carouselImages = document.querySelector('.carousel__images');
+const carouselButtons = document.querySelectorAll('.carousel__button');
+const numberOfImages = document.querySelectorAll('.carousel__images img').length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      if (event.target.id === 'previous') {
+        if (imageIndex !== 1) {
+          imageIndex--;
+          translateX += 750;
+        }
+      } else {
+        if (imageIndex !== numberOfImages) {
+          imageIndex++;
+          translateX -= 750;
+        }
+      }
+      
+      carouselImages.style.transform = `translateX(${translateX}px)`;
+      images.forEach((image, index) => {
+        if (index === imageIndex - 1) {
+          image.classList.add('active');
+        } else {
+          image.classList.remove('active');
+        }
+      });
+    });
+  });
